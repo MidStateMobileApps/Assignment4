bc2db79c4f92d82c2c0dd361c9bc14ff16e7ca6b
user132548 <user132548@MAC098.local>
2017-11-01 20:49:31 -0500

    Initial Commit

---
+++ GoSoon/GoSoon/GoSoonPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using Xamarin.Forms;
+
+namespace GoSoon
+{
+    public partial class GoSoonPage : ContentPage
+    {
+        public GoSoonPage()
+        {
+            InitializeComponent();
+            Label l = new Label()
+            {
+                Text = "Bubba!"
+            };
+            this.Content = l;
+            l.Font = Font.SystemFontOfSize(28);
+            l.HorizontalOptions = LayoutOptions.Center;
+            l.VerticalOptions = LayoutOptions.Center;
+        }
+    }
+}
